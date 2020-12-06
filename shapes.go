f84a0323ae9587e429ab92217d45991888dbdd41
koh97222 <koh97222@gmail.com>
2020-12-06 11:52:20 +0900

    メソッド

--- shapes.go
+++ shapes.go
@@ -1,9 +1,26 @@
 package main
 
+import "math"
+
 // Rectangle 四角形の構造体
 type Rectangle struct {
 	width  float64
 	height float64
+}
+
+// Circle 円の構造体
+type Circle struct {
+	radius float64
+}
+
+// Area 四角形の面積
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+// Area 円の面積
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
 }
 
 // Perimeter Perimeter
--- shapes_test.go
+++ shapes_test.go
@@ -15,12 +15,23 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	rectangle := Rectangle{10.0, 10.0}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
 
-	got := Area(rectangle)
-	want := 100.0
+		got := rectangle.Area()
+		want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f but got %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %g but got %g", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %g but got %g", got, want)
+		}
+	})
 }
