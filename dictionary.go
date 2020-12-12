d07d0154a7c8799770ce1fb60665e35e40f25f08
koh97222 <koh97222@gmail.com>
2020-12-12 15:28:45 +0900

    map2

--- dictionary.go
+++ dictionary.go
@@ -1,9 +1,20 @@
 package main
 
+import "errors"
+
 // Dictionary Dictionary
 type Dictionary map[string]string
 
+// ErrNotFound dictionaryのkeyが見つからず、正常にvalueを取れなかったとき。
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 // Search Search
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+
+	// map 2番目の戻り値はkeyが正常に検出されたかを判別する。（bool）
+	definication, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definication, nil
 }
--- dictionaty_test.go
+++ dictionaty_test.go
@@ -10,13 +10,36 @@ func assertStrings(t *testing.T, got, want string) {
 	}
 }
 
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
 func TestSearch(t *testing.T) {
 
 	// map[keyの型]valueの型{"hoge":"fuga"}
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+
+		_, got := dictionary.Search("unknown")
+
+		// if got == nil {
+		// 	t.Fatal("expected to get an error.")
+		// }
+
+		assertError(t, got, ErrNotFound)
+
+	})
+
 }
