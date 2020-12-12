0f5d4e0229eb058223b0cadda44850b1b86566eb
koh97222 <koh97222@gmail.com>
2020-12-13 07:46:34 +0900

    map3

--- dictionary.go
+++ dictionary.go
@@ -5,8 +5,18 @@ import "errors"
 // Dictionary Dictionary
 type Dictionary map[string]string
 
+// DictionaryErr DictionaryErr
+type DictionaryErr string
+
 // ErrNotFound dictionaryのkeyが見つからず、正常にvalueを取れなかったとき。
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound  = errors.New("could not find the word you were looking for")
+	ErrWordExist = errors.New("cannot add word because it already exists")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 // Search Search
 func (d Dictionary) Search(word string) (string, error) {
@@ -17,4 +27,23 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", ErrNotFound
 	}
 	return definication, nil
+}
+
+// Add 辞書にwordを加えます
+func (d Dictionary) Add(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		// mapは参照型なので、ポインタを渡さなくても変更できる。
+		// 基礎となるデータ構造の参照を保持します。
+		d[word] = definition
+	case nil:
+		return ErrWordExist
+	default:
+		return err
+	}
+
+	return nil
 }
--- dictionaty_test.go
+++ dictionaty_test.go
@@ -16,6 +16,27 @@ func assertError(t *testing.T, got, want error) {
 	if got != want {
 		t.Errorf("got error %q want %q", got, want)
 	}
+
+	if got == nil {
+		if want == nil {
+			return
+		}
+		t.Errorf("expected got an error")
+	}
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != definition {
+		t.Errorf("got %q want %q", got, definition)
+	}
 }
 
 func TestSearch(t *testing.T) {
@@ -40,6 +61,32 @@ func TestSearch(t *testing.T) {
 
 		assertError(t, got, ErrNotFound)
 
+	})
+
+}
+
+func TestAdd(t *testing.T) {
+
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExist)
+		assertDefinition(t, dictionary, word, definition)
 	})
 
 }
