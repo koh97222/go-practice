47d8ed9aced06eb3c0f1b812571c841d2abab380
koh97222 <koh97222@gmail.com>
2020-12-16 22:07:24 +0900

    スタブ・モック1

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countDownStart = 3
+
 // Hello ...
 func Hello() string {
 	return "Hello,World"
 }
 
 func main() {
-	// fmt.Println(Hello())
-	// Greet(os.Stdout, "Elodie")
+	Countdown(os.Stdout)
+}
+
+// Countdown Countdown
+func Countdown(out io.Writer) {
+
+	for i := countDownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
+
 }
